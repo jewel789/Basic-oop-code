320937dd09335155612153f7dc94c1a724f557fc
jewel789 <54429581+jewel789@users.noreply.github.com>
2023-01-27 03:19:03 +0600

    Create Student.java
    abstract class

---
+++ abstract/Student.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+
+
+abstract class Man{
+
+		public String name="Jewel Miah";
+		public int age =24;
+		public abstract void study(); // abstract methods can only in abstract class
+
+}
+
+
+public class Student extends Man{
+
+         public int graguate = 2021;
+
+         @Override
+         public void study(){
+         	System.out.println(" not hardcore academic stuying ");
+         } 
+       
+        public static void main(String[] args) {
+       		Student object1= new Student();
+
+       		System.out.println(object1.name);
+       		System.out.println(object1.age);
+       		System.out.println(object1.graguate);
+       	//	object1.study();	 
+
+       		object1.name="jewel";
+       		System.out.println(object1.name); 	
+         }
+}
